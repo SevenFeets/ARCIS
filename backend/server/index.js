a4dcba760564df76d8ac7a34d76eb0577f6fafa2
System dev <eronchik@gmail.com>
2025-05-24 15:22:10 +0300

    created backend initial tructure

---
+++ backend/server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const { v4: uuidv4 } = require("uuid");
+
+app.use(cors()); // enable cors
+app.use(helmet()); // secure headers
+app.use(express.json()); // parse json bodies in the request
+
+const app = express();
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+
+
+
